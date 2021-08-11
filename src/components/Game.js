45e569b212ae185af7b2a6f1661319fbdb6c9ddc
tabbykatz <tomelay@gmail.com>
2021-08-10 17:26:01 -0700

    add changes to main for vercel

--- src/components/Game.js
+++ src/components/Game.js
@@ -15,11 +15,13 @@ const [stepNumber, setStepNumber] = useState(0);
 const [xIsNext, setXisNext] = useState('true');
 const winner = calculateWinner(history[stepNumber]);
 
+
 const handleClick = (i) => {
   const timeInHistory = history.slice(0, stepNumber + 1);
   const current = timeInHistory[stepNumber];
   const squares = [...current];
   //when user clicks a square that is already used or game is won, return
+ 
   if (winner || squares[i]) return;
   squares[i] = xIsNext ? 'X' : '0';
   setHistory([...timeInHistory, squares]);
@@ -34,7 +36,7 @@ const jumpTo = (step) => {
 
 const renderMoves = () => (
   history.map((_step, move) => {
-    const destination = move ? `Go back by ${move}` : 'Go to beginning';
+    const destination = move ? `Back to turn ${move}` : 'Go to beginning';
     return (
       <li key={move}>
         <button className='timetravel' onClick={() => jumpTo(move)}>{destination}</button>
@@ -49,7 +51,7 @@ const renderMoves = () => (
 
       <Board squares={history[stepNumber]} onClick={handleClick} />
       <div style={styles}>
-        <p>{winner ? winner + ' wins!': 'It\'s ' + (xIsNext ? 'X' : 'O') + `'s turn`}</p>
+        <p>{winner ? winner + ' wins!' : stepNumber > 8 ? 'GAME OVER!' : 'It\'s ' + (xIsNext ? 'X' : 'O') + `'s turn`}</p>
         {renderMoves()}
       </div>
     </>
